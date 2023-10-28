f608ed2f658796a3a73fdf9558c01bedd98b17ab
fahinurhaquefima <fahinurhaquef@gmail.com>
2023-10-28 22:56:22 +0600

    javascript besic practice

---
+++ index.js
@@ -0,0 +1,54 @@
+//-------------User input value diye Arithmetic Operators------------ 
+
+// let num1 = prompt("Enter Your first number");
+// let num2 = prompt("Enter Your second number");
+
+// num1 = parseInt(num1)
+// num2 = parseInt(num2)
+
+// let sum = num1 + num2;
+// let sub = num1 - num2;
+// let mul = num1 * num2;
+// let div = num1 / num2;
+// let rem = num1 % num2;
+
+// document.write("Addition is " + sum + "<br/>") 
+// document.write("Subtraction is " + sub + "<br/>")
+// document.write("Multiplication is " + mul + "<br/>")
+// document.write("Division is " + div + "<br/>")
+// document.write("Remainder is " + rem);
+
+// ----------Area of various shapes--------------
+
+// let base = prompt("Enter Base");
+// let height = prompt("Enter Height");
+
+// let area = base * height;
+// let triangle = (base * height)/2;
+
+// document.write("Square is " + area + "<br/>") //print  square/rectengle shapes
+// document.write("Triangle is " + triangle + "<br/>") //print  Triangle shapes
+
+// let a = prompt("Enter a");
+// let b = prompt("Enter b");
+// let height = prompt("Enter Height");
+
+// let trapezium = ((a+b) * height)/2;
+// document.write("Trapezium is " + trapezium + "<br/>") //print  trapezium shape
+
+//-------------temperature converter--------------
+
+// let fahrn = prompt("Enter Fahrenhiet:")
+// let cels = (fahrn - 32) * (5/9);
+
+// document.write("Fahrenhiet is = " + cels) //Print fahrenhiet to celsius calculator
+
+// let cels = prompt("Enter Celsius :")
+// let fahrn = (cels * (9/5)) + 32;
+
+// document.write("Celsius is = " + fahrn) //Print Celsius to fahrenhiet calculator
+
+// let kel = prompt("Enter Kelvin:")
+// let fahrn = ((kel - 273.15) * (9/5) + 32);
+// document.write("Kelvin is = " + fahrn) //Print kelvin to fahrenheit calculator
+
